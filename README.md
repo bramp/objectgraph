4f8b0aa2ecbc94bceb15680efedc4fbfcf584162
Andrew Brampton <me@bramp.net>
2014-08-03 12:41:35 -0400

    Initial commit of project.

--- README.md
+++ README.md
@@ -1,4 +1,80 @@
 objectgraph
 ===========
+by Andrew Brampton 2014
 
-Simple library to traverse Java object graph
+Simple library to traverse Java object graph. A breadth-first search is conducted
+on each field of each object encountered. No object is visited twice, and the
+algorithm is array based, so does not encounter stack overflow issues.
+
+Maven
+-----
+
+	<dependency>
+		<groupId>net.bramp.objectgraph</groupId>
+		<artifactId>objectgraph</artifactId>
+		<version>1.0</version>
+	</dependency>
+
+Example
+-------
+
+	ObjectGraph
+	    .visitor(new ObjectGraph.Visitor() {
+		    @Override
+		    public boolean visit(Object object, Class clazz) {
+			    System.out.println("visited " + object.toString());
+			    return false;
+		    }
+	    })
+	    .excludeStatic()
+	    .excludeTransient()
+	    .traverse( myObject );
+
+Use Cases
+---------
+
+We were encountering an issue where Hibernate entities were accidentally being
+stored in a HTTP session. This can cause a number of issues. To help alert new
+developers to this issue, a Servlet filter was created that during development
+would search the session for any Hibernate entities. If a entity was found, a
+friendly message would be displayed to encourage the developer to fix the
+problem.
+
+This library has numerous other uses cases, however, in its current form the
+API is not well suited for serialising a object graph
+
+Build and Release
+-----------------
+
+To build this project use ``mvn``.
+
+To push a release to maven central use the standard maven release plugin, and Sonatype's OSS repo:
+
+	mvn release:prepare
+	mvn release:perform
+
+Licence
+-------
+	Copyright (c) 2014, Andrew Brampton
+	All rights reserved.
+	
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions are met:
+	
+	* Redistributions of source code must retain the above copyright notice, this
+	  list of conditions and the following disclaimer.
+	
+	* Redistributions in binary form must reproduce the above copyright notice,
+	  this list of conditions and the following disclaimer in the documentation
+	  and/or other materials provided with the distribution.
+	
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+	FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+	DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+	SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+	OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
---
+++ src/main/java/net/bramp/objectgraph/ObjectGraph.java
@@ -0,0 +1,138 @@
+package net.bramp.objectgraph;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.*;
+
+/**
+ * Uses a breath-first search to transverse a object graph.
+ * This is done a similar manner to how a library would serialise a object
+ *
+ * @author bramp
+ */
+public class ObjectGraph {
+
+	final Map<Object, Class> visited = new IdentityHashMap<Object, Class>();
+	final Queue<Object> toVisit = new ArrayDeque<Object>();
+
+	final Visitor visitor;
+
+	boolean excludeTransient  = true;
+	boolean excludeStatic     = true;
+
+	public static interface Visitor {
+		/**
+		 * Called for each Object visited
+		 * @param object The object being visited
+		 * @param clazz The field type this object was in. This will differ from object.getClass(), when the Clazz is one of the primitive types
+		 * @return return true if you wish the graph transversal to stop, otherwise it will continue.
+		 */
+		public boolean visit(Object object, Class clazz);
+	}
+
+	ObjectGraph(Visitor visitor) {
+		this.visitor = visitor;
+	}
+
+	static ObjectGraph visitor(Visitor visitor) {
+		return new ObjectGraph(visitor);
+	}
+
+	public ObjectGraph includeTransient() {
+		excludeTransient = false;
+		return this;
+	}
+
+	public ObjectGraph excludeTransient() {
+		excludeTransient = true;
+		return this;
+	}
+
+	public ObjectGraph includeStatic() {
+		excludeStatic = false;
+		return this;
+	}
+
+	public ObjectGraph excludeStatic() {
+		excludeStatic = true;
+		return this;
+	}
+
+	/**
+	 * Conducts a breath first search of the object graph
+	 * @param root
+	 */
+	public void traverse(Object root) {
+		// Reset the state
+		visited.clear();
+		toVisit.clear();
+
+		if (root == null)
+			return;
+
+		addIfNotVisited(root, root.getClass());
+		start();
+	}
+
+	private boolean canDecend(Class clazz) {
+		// We can't decend into Primitives (they are not objects)
+		return !clazz.isPrimitive();
+	}
+
+	private void addIfNotVisited(Object object, Class clazz) {
+		if (object != null && !visited.containsKey(object)) {
+			toVisit.add(object);
+			visited.put(object, clazz);
+		}
+	}
+
+	private void start() {
+
+		while (!toVisit.isEmpty()) {
+
+			Object obj = toVisit.remove();
+			Class clazz = visited.get(obj);
+
+			boolean terminate = visitor.visit(obj, clazz);
+			if (terminate)
+				return;
+
+			if (!canDecend(clazz))
+				continue;
+
+			if (clazz.isArray()) {
+				// If an Array, add each element to follow up
+				Class arrayType = clazz.getComponentType();
+
+				final int len = Array.getLength(obj);
+
+				for (int i = 0; i < len; i++) {
+					addIfNotVisited(Array.get(obj, i), arrayType);
+				}
+
+			} else {
+				// If a normal class, add each field
+				Field[] fields = clazz.getDeclaredFields();
+				for (Field field : fields) {
+					int modifiers = field.getModifiers();
+
+					if (excludeStatic && (modifiers & Modifier.STATIC) == Modifier.STATIC)
+						continue;
+
+					if (excludeTransient && (modifiers & Modifier.TRANSIENT) == Modifier.TRANSIENT)
+						continue;
+
+					try {
+						field.setAccessible(true);
+						Object value = field.get(obj);
+						addIfNotVisited(value, field.getType());
+
+					} catch (IllegalAccessException e) {
+						// Ignore the exception
+					}
+				}
+			}
+		}
+	}
+}
